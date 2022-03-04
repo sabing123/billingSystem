f99e3f0a1412ad7f783cb2e8f8cc8dfc6963b202
Sabin <sabingautam05@gmail.com>
2022-03-04 21:54:29 +0545

    complete merging into master

--- billingsystem/static/js/navigation.js
+++ billingsystem/static/js/navigation.js
@@ -9,10 +9,4 @@ function closeNav() {
   document.getElementById("main").style.marginLeft= "0";
 }
 
-date = new Date();
-year = date.getFullYear();
-month = date.getMonth() + 1;
-day = date.getDate();
-document.getElementById("current_date").innerHTML = month + "/" + day + "/" + year;
-
 
